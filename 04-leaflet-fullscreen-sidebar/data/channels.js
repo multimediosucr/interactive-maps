9c980bae19783b99380613893cbfac06a7fa0994
yafred <fred599@laposte.net>
2017-01-11 00:42:43 +0100

    update philippe-croom

--- 04-leaflet-fullscreen-sidebar/data/channels.js
+++ 04-leaflet-fullscreen-sidebar/data/channels.js
@@ -4,5 +4,6 @@ channels = [
 	{ 'id': 'franceimage', 'title': 'FranceImage', 'url': 'data/franceimage-markerlist.json' },
 	{ 'id': 'village-prefere-des-francais', 'title': 'Le Village préféré des Français', 'url': 'data/village-prefere-des-francais-markerlist.json' },
 	{ 'id': 'monument-prefere-des-francais', 'title': 'Le Monument préféré des Français', 'url': 'data/monument-prefere-des-francais-markerlist.json' },
-	{ 'id': 'midi-en-france-suivez-le-guide', 'title': 'Midi en France - Suivez le Guide', 'url': 'data/midi-en-france-suivez-le-guide-markerlist.json' }
+	{ 'id': 'midi-en-france-suivez-le-guide', 'title': 'Midi en France - Suivez le Guide', 'url': 'data/midi-en-france-suivez-le-guide-markerlist.json' },
+	{ 'id': 'philippe-croom', 'title': 'Philippe Croom', 'url': 'data/philippe-croom-markerlist.json' }
 ];
