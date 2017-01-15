dc01975563c124a87e6ab3a2f36daffab90ccf78
yafred <fred599@laposte.net>
2017-01-15 17:14:56 +0100

    update franceimage channel img

--- 04-leaflet-fullscreen-sidebar/data/channels.js
+++ 04-leaflet-fullscreen-sidebar/data/channels.js
@@ -1,7 +1,7 @@
 ---
 ---
 channels = [
-	{ 'id': 'franceimage', 'title': 'FranceImage', 'url': 'data/franceimage-markerlist.json', 'thumbnail': '' },
+	{ 'id': 'franceimage', 'title': 'FranceImage', 'url': 'data/franceimage-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-ngN5pvT9n9Y/AAAAAAAAAAI/AAAAAAAAAAA/Vpx-EzqkPak/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'village-prefere-des-francais', 'title': 'Le Village préféré des Français', 'url': 'data/village-prefere-des-francais-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-g-1CJVenc6I/AAAAAAAAAAI/AAAAAAAAAAA/WRm02ojDq5M/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'monument-prefere-des-francais', 'title': 'Le Monument préféré des Français', 'url': 'data/monument-prefere-des-francais-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-k7AIZ_EV5RA/AAAAAAAAAAI/AAAAAAAAAAA/v8SAQFmKhSM/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
 	{ 'id': 'midi-en-france-suivez-le-guide', 'title': 'Midi en France - Suivez le Guide', 'url': 'data/midi-en-france-suivez-le-guide-markerlist.json', 'thumbnail': 'https://yt3.ggpht.com/-M1B6zdM6rWE/AAAAAAAAAAI/AAAAAAAAAAA/b8BK0_U1lCg/s88-c-k-no-mo-rj-c0xffffff/photo.jpg' },
